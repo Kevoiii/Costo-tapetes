d8ae8444c34ff5c81cac28326fd3e0922c823b1e
Kevoiii <pevk080607hgtrzva1@cbtis217.edu.mx>
2024-11-20 07:34:36 -0600

    Base

---
+++ Tapetes/Tapetes/Insertar.cs
@@ -0,0 +1,32 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: CC2_PC36
+ * Date: 20/11/2024
+ * Time: 07:25 a. m.
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace Tapetes
+{
+	/// <summary>
+	/// Description of Insertar.
+	/// </summary>
+	public partial class Insertar : Form
+	{
+		public Insertar()
+		{
+			//
+			// The InitializeComponent() call is required for Windows Forms designer support.
+			//
+			InitializeComponent();
+			
+			//
+			// TODO: Add constructor code after the InitializeComponent() call.
+			//
+		}
+	}
+}
---
+++ Tapetes/Tapetes/intro.cs
@@ -0,0 +1,32 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: CC2_PC36
+ * Date: 20/11/2024
+ * Time: 07:25 a. m.
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace Tapetes
+{
+	/// <summary>
+	/// Description of intro.
+	/// </summary>
+	public partial class intro : Form
+	{
+		public intro()
+		{
+			//
+			// The InitializeComponent() call is required for Windows Forms designer support.
+			//
+			InitializeComponent();
+			
+			//
+			// TODO: Add constructor code after the InitializeComponent() call.
+			//
+		}
+	}
+}
