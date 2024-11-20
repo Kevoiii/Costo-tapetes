0baa54f8bba1e1a280539c5c71df35f82a4c17cf
Kevoiii <pevk080607hgtrzva1@cbtis217.edu.mx>
2024-11-20 08:05:39 -0600

    Diseño introduccion

--- Tapetes/Tapetes/intro.Designer.cs
+++ Tapetes/Tapetes/intro.Designer.cs
@@ -36,7 +36,83 @@ namespace Tapetes
 		/// </summary>
 		private void InitializeComponent()
 		{
+			System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(intro));
+			this.button4 = new System.Windows.Forms.Button();
+			this.label1 = new System.Windows.Forms.Label();
+			this.pictureBox2 = new System.Windows.Forms.PictureBox();
+			this.button1 = new System.Windows.Forms.Button();
+			this.button2 = new System.Windows.Forms.Button();
+			this.button3 = new System.Windows.Forms.Button();
+			((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
 			this.SuspendLayout();
+			// 
+			// button4
+			// 
+			this.button4.BackColor = System.Drawing.Color.Yellow;
+			this.button4.Font = new System.Drawing.Font("Gill Sans Ultra Bold", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.button4.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+			this.button4.Location = new System.Drawing.Point(258, 80);
+			this.button4.Name = "button4";
+			this.button4.Size = new System.Drawing.Size(411, 49);
+			this.button4.TabIndex = 5;
+			this.button4.Text = "Primero que nada";
+			this.button4.UseVisualStyleBackColor = false;
+			// 
+			// label1
+			// 
+			this.label1.BackColor = System.Drawing.Color.Gold;
+			this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 21.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.label1.Location = new System.Drawing.Point(1, 214);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(922, 413);
+			this.label1.TabIndex = 6;
+			this.label1.Text = resources.GetString("label1.Text");
+			// 
+			// pictureBox2
+			// 
+			this.pictureBox2.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox2.Image")));
+			this.pictureBox2.Location = new System.Drawing.Point(1, 23);
+			this.pictureBox2.Name = "pictureBox2";
+			this.pictureBox2.Size = new System.Drawing.Size(205, 188);
+			this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+			this.pictureBox2.TabIndex = 8;
+			this.pictureBox2.TabStop = false;
+			// 
+			// button1
+			// 
+			this.button1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+			this.button1.Font = new System.Drawing.Font("Gill Sans Ultra Bold", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.button1.ForeColor = System.Drawing.Color.Yellow;
+			this.button1.Location = new System.Drawing.Point(375, 558);
+			this.button1.Name = "button1";
+			this.button1.Size = new System.Drawing.Size(179, 49);
+			this.button1.TabIndex = 9;
+			this.button1.Text = "Entrar";
+			this.button1.UseVisualStyleBackColor = false;
+			// 
+			// button2
+			// 
+			this.button2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+			this.button2.Font = new System.Drawing.Font("Gill Sans Ultra Bold", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.button2.ForeColor = System.Drawing.Color.Yellow;
+			this.button2.Location = new System.Drawing.Point(1, 558);
+			this.button2.Name = "button2";
+			this.button2.Size = new System.Drawing.Size(349, 49);
+			this.button2.TabIndex = 10;
+			this.button2.Text = "!La mejor app¡";
+			this.button2.UseVisualStyleBackColor = false;
+			// 
+			// button3
+			// 
+			this.button3.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+			this.button3.Font = new System.Drawing.Font("Gill Sans Ultra Bold", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.button3.ForeColor = System.Drawing.Color.Yellow;
+			this.button3.Location = new System.Drawing.Point(578, 558);
+			this.button3.Name = "button3";
+			this.button3.Size = new System.Drawing.Size(349, 49);
+			this.button3.TabIndex = 11;
+			this.button3.Text = "!Barato¡";
+			this.button3.UseVisualStyleBackColor = false;
 			// 
 			// intro
 			// 
@@ -44,9 +120,22 @@ namespace Tapetes
 			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
 			this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
 			this.ClientSize = new System.Drawing.Size(924, 619);
+			this.Controls.Add(this.button3);
+			this.Controls.Add(this.button2);
+			this.Controls.Add(this.button1);
+			this.Controls.Add(this.pictureBox2);
+			this.Controls.Add(this.label1);
+			this.Controls.Add(this.button4);
 			this.Name = "intro";
 			this.Text = "intro";
+			((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
 			this.ResumeLayout(false);
 		}
+		private System.Windows.Forms.Button button3;
+		private System.Windows.Forms.Button button2;
+		private System.Windows.Forms.Button button1;
+		private System.Windows.Forms.PictureBox pictureBox2;
+		private System.Windows.Forms.Label label1;
+		private System.Windows.Forms.Button button4;
 	}
 }
